46238801ed3a411d1d8f0b0e85499638351b9f41
casimp <c.a.simpson01@gmail.com>
2020-03-26 09:45:37 +0000

    Added reset_weights to tools

--- undt_ai/tools.py
+++ undt_ai/tools.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 
 def rmse(y_true, y_pred):
     from tensorflow.keras import backend as K
@@ -15,14 +16,24 @@ def add_noise(X, level=0.05):
     X_noise = X + np.max(X, axis=1)[:, :, None] * noise
     return X_noise
 
-def noise_augment(X, y: list, levels=[0, 0.01, 0.02, 0.04]):
-    """ Adugment dataset with white noise of varying levels (wrt.
+def noise_augment(x, y: list, levels=[0.02, ]):
+    """ Augment dataset with white noise of varying levels (wrt.
      the peak signal.) """
-    X_aug = []
+    x_aug = []
     y_aug = [np.vstack([i]*len(levels)) for i in y]
 
     for level in levels:
-        X_aug.append(add_noise(X, level))
-    return [np.vstack(X_aug)] + y_aug
+        x_aug.append(add_noise(x, level))
+    return [np.vstack(x_aug)] + y_aug
 
-
+def reset_weights(model):
+  for layer in model.layers: 
+    if isinstance(layer, tf.keras.Model):
+      reset_weights(layer)
+      continue
+    for k, initializer in layer.__dict__.items():
+      if "initializer" not in k:
+        continue
+      # find the corresponding variable
+      var = getattr(layer, k.replace("_initializer", ""))
+      var.assign(initializer(var.shape, var.dtype))
